386f3b35a27eb8a225b9088ecc1fe949470c3351
lei11355 <36304559+lei11355@users.noreply.github.com>
2022-03-11 12:49:34 -0500

    Add files via upload

--- src/page-template.js
+++ src/page-template.js
@@ -1,1 +1,57 @@
 
+module.exports = templateData => {
+  // destructure page data by section
+  const { name, description, installation, usage,contribution,test,licenseType,projects,confirmAddProject,...header } = templateData;
+  console.log(templateData);
+
+  // const licensearry = animalArr.map(licensearry => {
+  //   console.log(licenseType);
+  //   return `${licenseType} are really cool animals.`;
+  // });
+  return `
+                                  ${name}
+   Description  
+   ${description}
+   
+   
+   
+   Installation 
+  ${installation}
+
+
+
+   userage  
+   ${usage}
+
+
+
+   Contribution guideline 
+    ${contribution}
+   
+   
+    Testing   
+    ${test}
+
+    License
+    ${licenseType}
+
+    Link
+    ${generateLink(projects)}
+    
+    `}
+   
+   
+    const generateLink = x => {
+      return `
+     ${x.map(({  link }) => {
+       console.log(link)
+            return `${link};
+        `
+      }
+     )}`};
+    //  
+   
+
+
+ 
+
